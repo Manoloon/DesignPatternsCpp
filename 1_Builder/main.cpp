cbb938e03349e80d867cc6a7c99e80da3b007b6c
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2024-02-09 19:05:08 +0100

    Builder assigment completed

--- 1_Builder/main.cpp
+++ 1_Builder/main.cpp
@@ -1,28 +1,35 @@
 #include <string>
+#include <iostream>
 #include <ostream>
+#include <vector>
 using namespace std;
 
 class CodeBuilder
 {
 public:
-    CodeBuilder(const string& class_name)
-    {
-        string Class_name;
-    }
-
+    CodeBuilder(const string& class_name):Class_name(class_name){}
+    string Class_name;
+    vector<pair<string,string>> data;
+    
     CodeBuilder& add_field(const string& name, const string& type)
     {
-
+        data.push_back(make_pair(name,type));
+        return *this;
     }
 
     friend ostream& operator<<(ostream& os, const CodeBuilder& obj)
     {
-        // todo
+        os << "class " << obj.Class_name << std::endl << "{" << std::endl;
+        for(const auto& d : obj.data){
+            os  << "  "<< d.second << " " << d.first << ";" << std::endl;
+            }
+        os << "};" << std::endl;
+        return os; 
     }
 };
 
 int main(){
-    auto cb = CodeBuilder{"Person"}.add_field("name", "string").add_field("age", "int");
+    auto cb = CodeBuilder{"Pablo"}.add_field("name", "string").add_field("age", "int");
     cout << cb;
 return 0;
 }
