a6fcc860063642108868ac514a8125d8c83da0f3
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2024-02-17 07:25:59 +0100

    update

--- 2_Factory/FM_Points.h
+++ 2_Factory/FM_Points.h
@@ -5,15 +5,15 @@
 #include <cmath>
 
 class FM_Points{
-      FM_Points(float x,float y):x(x),y(y){}  
+      FM_Points(double x,double y):x(x),y(y){}  
 public:
-    float x;
-    float y;
+    double x;
+    double y;
 
-    static FM_Points NewCartesian(float x, float y){
+    static FM_Points NewCartesian(double x, double y){
         return {x,y};
     }
-    static FM_Points NewPolar(float radius, float theta){
+    static FM_Points NewPolar(double radius, double theta){
         return {radius * cos(theta),radius * sin(theta)};
     }
     friend std::ostream &operator <<(std::ostream &os, const FM_Points& point){
