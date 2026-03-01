2099e30b3d15fcb4da58bdd9294d57aefc7482fa
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2026-03-01 16:29:01 +0100

    proxy

--- 12_Proxy/main.cpp
+++ 12_Proxy/main.cpp
@@ -21,12 +21,37 @@ public:
 
 class ResponsiblePerson
 {
-    ResponsiblePerson(const Person &person) : person(person) {}
-    // todo
+  public:
+  ResponsiblePerson(const Person &person) : person(person){}
+  int get_age() const {return person.age;}
+  void set_age(int age) { person.set_age(age);}
+  std::string drink()
+  {
+    if (person.age < 18)
+    {
+      return "too young";
+    }
+    return person.drink();
+  }
+  std::string drive()
+  {
+    if (person.age < 16)
+    {
+      return "too young";
+    }
+    return person.drive();
+  }
+  std::string drink_and_drive()
+  {
+    return "dead";
+  }
 private:
     Person person;
 };
 int main()
 {
+  Person person1(16);
+  ResponsiblePerson rp(person1);
+  std::string dr = rp.drive();
 	return 0;
 }
