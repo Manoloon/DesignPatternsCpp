cf0f65489a42a1179f5130d35f70089da96881db
Manoloon <pablo.r.sanchez@hotmail.com>
2026-03-20 10:54:51 +0100

    command exercise

--- 14_Command/main.cpp
+++ 14_Command/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <typeinfo>
 #include <cassert>
 /*
-	Responsability Exercise 
+	command Exercise 
 */
+struct Command
+{
+  enum Action { deposit, withdraw } action;
+  int amount{0};
+  bool success{false};
+};
 
+struct Account
+{
+  int balance{0};
+
+  void process(Command& cmd)
+  {
+    // todo
+  }
+};
 
 int main()
 {
