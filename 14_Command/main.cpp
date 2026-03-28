af20817476222a4e23e15c2fa25a6d09ce18c9a1
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2026-03-28 11:40:08 +0100

    command exercise finished

--- 14_Command/main.cpp
+++ 14_Command/main.cpp
@@ -9,19 +9,44 @@ struct Command
   enum Action { deposit, withdraw } action;
   int amount{0};
   bool success{false};
+  Command(Action act, int amount):action{act},amount{amount}{};
 };
-
 struct Account
 {
   int balance{0};
 
   void process(Command& cmd)
   {
-    // todo
+    if (cmd.success)
+    {
+      return;
+    }
+    if (cmd.action == cmd.deposit)
+    {
+        balance += cmd.amount;
+        cmd.success = true;
+    }
+    else 
+    {
+        if (balance >= cmd.amount)
+        {
+            balance -= cmd.amount;
+            cmd.success = true;
+        }
+    }
   }
 };
 
 int main()
 {
-
+  Account account;
+  Command cmd1(Command::deposit,100);
+  account.process(cmd1);
+  assert(account.balance == 100);
+  Command cmd2(Command::withdraw,100);
+  account.process(cmd2);
+  assert(account.balance == 0);
+  Command cmd3(Command::withdraw,100);
+  account.process(cmd3);
+  assert(account.balance == 0);
 }
