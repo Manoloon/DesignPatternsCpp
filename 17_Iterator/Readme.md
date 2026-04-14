3e1d59281d4727e81a646248d5d169a1ec35b39c
Manoloon <pablo.r.sanchez@hotmail.com>
2026-04-14 15:30:45 +0200

    iterator

--- 17_Iterator/Readme.md
+++ 17_Iterator/Readme.md
@@ -3,3 +3,6 @@
 	Facilitate Traversal Data
 #Exercise
 	*Iterator Coding Exercise
+
+Given the following definition of a Node<T>, please implement preorder traversal that returns a sequence of Ts. 
+I have greatly simplified the problem by adding an accumulator argument into the preorder_traversal()  function.
--- 17_Iterator/main.cpp
+++ 17_Iterator/main.cpp
@@ -1,101 +1,68 @@
-#include <iostream>
-#include <map>
-#include <string>
-#include <cctype>
-#include <cassert>
 /*
-	Interpreter Exercise 
+	Iterator Exercise 
 */
-struct ExpressionProcessor
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+struct Node
 {
-  std::map<char,int> variables;
-  
-  int calculate(const std::string& expression)
-  {
-    int result = 0;
-    int sign = 1; // 1 == + , -1 == - ;
-    for(size_t i = 0; i < expression.size();)
+    T value;
+    Node *left{nullptr}, *right{nullptr}, *parent{nullptr};
+
+    Node(T value) : value(value) {}
+
+    Node(T value, Node<T> *left, Node<T> *right) : value(value), left(left), right(right) {
+      left->parent = right->parent = this;
+    }
+
+    // traverse the node and its children preorder
+    // and put all the results into `result`
+    void preorder_traversal(vector<Node<T>*>& result)
     {
-      //auto curr = expression[i];
-      if(std::isspace(expression[i]))
+      Node<T>* current = this;
+      if(current->right)
       {
-        ++i;
-        continue;
-      }
-      // operators 
-      if(expression[i] == '+')
-      {
-        sign = 1;
-        ++i;
-      }
-      else if (expression[i] == '-')
-      {
-        sign = -1;
-        ++i;
-      }
-      // numbers
-      else if (std::isdigit(expression[i]))
-      {
-        int val = 0;
-        while(i < expression.size() && std::isdigit(expression[i]))
+        current = current->right;
+        while (current->left)
         {
-          val = val * 10 + (expression[i] - '0');
-          ++i;
+          current = current->left;
         }
-        result += sign * val;
-      }
-      // handle variables
-      else if (std::isalpha(expression[i]))
-      {
-        if(i + 1 < expression.size() && std::isalpha(expression[i+1]))
-        {
-          return 0;
-        }
-        char var = expression[i];
-        if(variables.find(var) == variables.end())
-        {
-          return 0;
-        }
-        result += sign * variables[var];
-        ++i;
       }
       else
       {
-        return 0;
+        Node<T>* p = current->parent;
+        while (p && current == p->right)
+        {
+          current = p;
+          p = current->parent;
+        }
+        current = p;
+        result.emplace_back(this);
       }
     }
-    return result;
-  }
 };
 
 int main()
 {
-  ExpressionProcessor ep;
-  ep.variables['x'] = 5;
-  ep.variables['y'] = 3;
-  std::cout << "Start test : where x = 5 and y = 3:\n";
-
-  auto test = ep.calculate("1");
-  assert(test == 1);
-  std::cout << "test only one digit: " << test << '\n';
-
-  test = ep.calculate("1+2+3");
-  assert ( test == 6);
-  std::cout << "test 1+2+3 : " << test << '\n';
-  
-  test = ep.calculate("10+x-y");
-  assert (test == 12);
-  std::cout << "test 10+x-y: " << test << '\n';
-
-  test = ep.calculate("x+z");
-  assert(test == 0);      
-  std::cout << "test x+z, z doesnt exists: " << test << '\n';
-
-  test = ep.calculate("ab+1");
-  assert(test == 0);   
-  std::cout << "test ab+1, invalid variables: " << test << '\n';
-
-  test = ep.calculate("1++2");
-  assert(test == 0);      // 0 (invalid parsing)
-  std::cout << "test 1++2, invalid parsing: " << test << '\n';
+  vector<Node<string>*> result;
+  vector<Node<string>*> input{new Node<string>{"a"},new Node<string>{"b"},new Node<string>{"c"},new Node<string>{"d"}};
+  for(auto n : input)
+  {
+    cout << (*n).value <<  '\n';
+    (*n).preorder_traversal(result);
+  }
+  cout << "print results\n";
+  if(result.empty())
+  {
+    cout << "result is empty\n";
+  }
+  else
+  {
+    for(const auto& n : result)
+    {
+        cout << n->value << '\n';
+    }
+  }
 }
